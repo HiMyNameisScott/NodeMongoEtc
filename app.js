b81c69faf14200e6b3afa4d81f667dcbf9d9981e
Scott Koss <scotttkoss@gmail.com>
2020-06-24 13:04:13 -0700

    Simple Nod Example

---
+++ app.js
@@ -0,0 +1,21 @@
+const rect = {
+    perimeter: (x, y) => 2 * (x + y),
+    area: (x, y) => x * y
+};
+
+function solveRect(l, w) {
+    console.log(`Solving for rectangle with dimensions ${l}, ${w}`);
+
+    if (l <= 0 || w <=0) {
+        console.log(`Rectangle dimensions should be greater then 0
+                        the values passed in were: ${l}, ${w};`)
+    } else {
+        console.log(`Area of rectangle: ${rect.area(l, w)}` )
+        console.log(`Area of rectangle: ${rect.area(l, w)}`);
+    }
+}
+
+solveRect(2,3);
+solveRect(4,8);
+solveRect(0,7);
+solveRect(-3, 5);
